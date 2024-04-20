feb9520834bc46fe9313f9b28f230d6f6cfb5252
jicking <7297029+jicking@users.noreply.github.com>
2024-04-20 21:51:44 +0800

    Add project files.

---
+++ ConsoleApp1/Calculator.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1;
+
+public static class Calculator
+{
+    public static int Sum(int num1, int num2) => num1 + num2;
+
+    public static int Multiply(int num1, int num2) => num1 + num2;
+}
---
+++ ConsoleApp1Tests/CalculatorTests.cs
@@ -0,0 +1,25 @@
+﻿using Xunit;
+using ConsoleApp1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1.Tests
+{
+    public class CalculatorTests
+    {
+        [Fact()]
+        public void SumTest()
+        {
+            Xunit.Assert.Fail("This test needs an implementation");
+        }
+
+        [Fact()]
+        public void MultiplyTest()
+        {
+            Xunit.Assert.Fail("This test needs an implementation");
+        }
+    }
+}
